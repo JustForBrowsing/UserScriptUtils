1f8b7d53f6f5a1a5e2d8beec4280273d2de65a37
JustForBrowsing <142632714+JustForBrowsing@users.noreply.github.com>
2025-03-14 04:18:59 -0500

    Update UserScriptUtils.js

--- UserScriptUtils.js
+++ UserScriptUtils.js
@@ -1,7 +1,7 @@
 /* 
 
 */
-/* Include the following lines in your ==UserScript== section
+/* Include the following lines in your ==UserScript== block
 
 // @require          https://cdn.jsdelivr.net/npm/eruda@3.4.1/eruda.min.js#sha256-bfOAXaBm8tuuqlR7TKg/pcfBDKi2ukNXsIl788w7mh8=
 // @require          https://cdn.jsdelivr.net/npm/eruda-code@2.2.0/eruda-code.min.js#sha256-QKv2Ow4Dvamh4teg/CpaSA0drpNKyqVUDv4bn0J8a78=
@@ -10,25 +10,31 @@
 // @grant            GM_getValue
 // @grant            GM_setValue
 // @grant            GM_addStyle
-*/
-'use strict';
-// const appId = GM_info.script.name;
-eruda.init({
-  default: {
-    transparency: 0.95,
-     displaySize: 55,
-           theme: 'Dark',
-  }
-});
-eruda.add(erudaCode);
-eruda.add(erudaMonitor);
-eruda.add(erudaTiming);
-eruda.position({x: 5, y: window.screen.height * 0.3;});
 
-// Replace normal console with the eruda console (for the UserScript window, at least).
-window.console = eruda.get('console');
-console.log(`%cUserScriptUtils: initialized.`, 'color:#4060FF;');
+*/
+// Include the following line after the ==UserScript== block to make eslint shut up about eruda:
+/* global eruda, erudaCode, erudaMonitor, erudaTiming */
+
+// 'use strict';
+if (windows?.eruda != null) {
+  eruda.init({
+    default: {
+      transparency: 0.95,
+       displaySize: 55,
+             theme: 'Dark',
+    }
+  });
+  eruda.add(erudaCode);
+  eruda.add(erudaMonitor);
+  eruda.add(erudaTiming);
+  eruda.position({x: 5, y: window.screen.height * 0.3;});
+  
+  // Replace normal console with the eruda console (for the UserScript window, at least).
+  window.console = eruda.get('console');
+}
 
 // I can't remember why I wanted/needed this...
 // (I think it's some kind of 'fix' for iPad Safari):
 document.addEventListener("touchstart", function() {}, false);
+
+console.log(`%cUserScriptUtils: initialized.`, 'color:#4060FF;');
