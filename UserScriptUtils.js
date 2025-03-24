df494ebf0ae39673d867f21411f1cdd8482487ca
JustForBrowsing <142632714+JustForBrowsing@users.noreply.github.com>
2025-03-24 18:46:51 -0500

    Update UserScriptUtils.js

--- UserScriptUtils.js
+++ UserScriptUtils.js
@@ -1,17 +1,18 @@
-/* 
+/** 
+  * @module UserScriptUtils
+  */
 
-*/
 /* Include the following lines in your ==UserScript== block
-
-// @require          https://cdn.jsdelivr.net/npm/eruda@3.4.1/eruda.min.js#sha256-bfOAXaBm8tuuqlR7TKg/pcfBDKi2ukNXsIl788w7mh8=
-// @require          https://cdn.jsdelivr.net/npm/eruda-code@2.2.0/eruda-code.min.js#sha256-QKv2Ow4Dvamh4teg/CpaSA0drpNKyqVUDv4bn0J8a78=
-// @require          https://cdn.jsdelivr.net/npm/eruda-monitor@1.1.1/eruda-monitor.min.js#sha256-7HNTeKKc32BEABLUmFkVDlDwYVIStEWenCnBfRSkaM4=
-// @require          https://cdn.jsdelivr.net/npm/eruda-timing@2.0.1/eruda-timing.min.js#sha256-PP95GJLgXsyqfEWOWl9d2DPDsoqUBl54vtczCjmS0Q0=
-// @grant            GM_getValue
-// @grant            GM_setValue
-// @grant            GM_addStyle
-
+  // @require     https://raw.githubusercontent.com/JustForBrowsing/UserScriptUtils/refs/heads/main/UserScriptUtils.js
+  // @require     https://cdn.jsdelivr.net/npm/eruda@3.4.1/eruda.min.js#sha256-bfOAXaBm8tuuqlR7TKg/pcfBDKi2ukNXsIl788w7mh8=
+  // @require     https://cdn.jsdelivr.net/npm/eruda-code@2.2.0/eruda-code.min.js#sha256-QKv2Ow4Dvamh4teg/CpaSA0drpNKyqVUDv4bn0J8a78=
+  // @require     https://cdn.jsdelivr.net/npm/eruda-monitor@1.1.1/eruda-monitor.min.js#sha256-7HNTeKKc32BEABLUmFkVDlDwYVIStEWenCnBfRSkaM4=
+  // @require     https://cdn.jsdelivr.net/npm/eruda-timing@2.0.1/eruda-timing.min.js#sha256-PP95GJLgXsyqfEWOWl9d2DPDsoqUBl54vtczCjmS0Q0=
+  // @grant       GM_getValue
+  // @grant       GM_setValue
+  // @grant       GM_addStyle
 */
+
 // Include the following line after the ==UserScript== block to make eslint shut up about eruda:
 /* global eruda, erudaCode, erudaMonitor, erudaTiming */
 
@@ -27,7 +28,8 @@
   eruda.add(erudaCode);
   eruda.add(erudaMonitor);
   eruda.add(erudaTiming);
-  eruda.position({ x: 5, y: window.screen.height / 3 });
+  eruda.position({ x: 5, 
+                   y: window.screen.height / 3 });
   eruda.get().config.set('displaySize', 55);
   
   // Replace normal console with the eruda console (for the UserScript window, at least).
