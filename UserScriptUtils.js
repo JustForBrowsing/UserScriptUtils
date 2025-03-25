e916685af7c7537ee66c8d1ea3697b78ea90e2c3
JustForBrowsing <142632714+JustForBrowsing@users.noreply.github.com>
2025-03-25 15:52:10 -0500

    Update UserScriptUtils.js

--- UserScriptUtils.js
+++ UserScriptUtils.js
@@ -25,6 +25,7 @@
 // @require     https://cdn.jsdelivr.net/npm/eruda-monitor@1.1.1/eruda-monitor.min.js#sha256-7HNTeKKc32BEABLUmFkVDlDwYVIStEWenCnBfRSkaM4=
 // @require     https://cdn.jsdelivr.net/npm/eruda-timing@2.0.1/eruda-timing.min.js#sha256-PP95GJLgXsyqfEWOWl9d2DPDsoqUBl54vtczCjmS0Q0=
 
+
 //let testVar = "nope";
 // console.log("Starting UserScriptUtils:" + testVar);
 (function () {
